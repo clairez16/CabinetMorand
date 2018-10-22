f1ef8b8d23c1cff5c7f12d16cdb2c2b74a661c4e
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-10-22 16:25:13 +0200

    const instead of let in the js file

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -7,9 +7,9 @@ window.onscroll = function() {myFunction()};
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  let nav = document.getElementById('navbar');
-  let body = document.body
-  let sticky = nav.offsetTop;
+  const nav = document.getElementById('navbar');
+  const body = document.body
+  const sticky = nav.offsetTop;
   body.classList.add("when-scroll")
   if (window.pageYOffset >= sticky) {
     nav.classList.add("sticky")
