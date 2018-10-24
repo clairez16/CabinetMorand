3726851788eeb23efa569635ecfc46e8c065c0d2
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-10-24 14:43:03 +0200

    banniere now ok, just logo to change of color with Photoshop

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -8,9 +8,9 @@ window.onscroll = function() {myFunction()};
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   const nav = document.getElementById('navbar');
-  const body = document.body
+  const banniere = document.getElementById('banniere')
   const sticky = nav.offsetTop;
-  body.classList.add("when-scroll")
+  banniere.classList.add("when-scroll")
   if (window.pageYOffset >= sticky) {
     nav.classList.add("sticky")
   } else {
